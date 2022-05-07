29bd3e0e4d5f86ad75b050dea2f2a3768e3281c8
Arthur <r.thur.light@gmail.com>
2022-05-07 12:31:09 +0000

    0.0.2

--- app/Http/Controllers/api/TransfertController.php
+++ app/Http/Controllers/api/TransfertController.php
@@ -8,7 +8,6 @@ use App\Models\Transfert;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class TransfertController extends Controller
 {
